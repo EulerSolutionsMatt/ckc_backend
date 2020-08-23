8a59a8677902491e31d33f7cce0bd5026e16a645
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2020-08-23 12:03:45 +0100

    update login() to send user Obj & remove password confirmation

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -16,28 +16,23 @@ class ApiAuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-//            'type' => 'integer',
         ]);
         if ($validator->fails())
         {
             return response(['errors'=>$validator->errors()->all()], 422);
         }
-
         $request['password']=Hash::make($request['password']);
         $request['remember_token'] = Str::random(10);
-//        $request['type'] = $request['type'] ? $request['type'] : 0;
         $user = User::create($request->toArray());
         $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-
-        $response = ['token' => $token, 'user' => $user];
-
+        $response = ['token' => $token];
         return response($response, 200);
     }
 
     public function login (Request $request) {
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6|confirmed',
+            'password' => 'required|string|min:6',
         ]);
         if ($validator->fails())
         {
@@ -47,7 +42,9 @@ class ApiAuthController extends Controller
         if ($user) {
             if (Hash::check($request->password, $user->password)) {
                 $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['token' => $token];
+                $response = [];
+                $response['token'] = $token;
+                $response['user'] = $user;
                 return response($response, 200);
             } else {
                 $response = ["message" => "Password mismatch"];
