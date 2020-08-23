fb2d6fa10aca0636826fa3440a96e407215527b0
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2020-08-23 18:02:37 +0100

    small refactor to routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,34 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// public routes
 Route::group(['middleware' => ['cors', 'json.response']], function () {
-    // public routes
     Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
-    Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+    Route::post('/register', 'Auth\ApiAuthController@register')->name('register.api');
     Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::get('products', 'ProductController@getAllProducts');
+    Route::get('products/{id}', 'ProductController@getProduct');
+    Route::post('products', 'ProductController@createProduct');
+    Route::put('products/{id}', 'ProductController@updateProduct');
+    Route::delete('products/{id}', 'ProductController@deleteProduct');
 });
 
+// protected routes
 Route::middleware('auth:api')->group(function () {
-    // our routes to be protected will go in here
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
-Route::get('products', 'ProductController@getAllProducts');
-Route::get('products/{id}', 'ProductController@getProduct');
-Route::post('products', 'ProductController@createProduct');
-Route::put('products/{id}', 'ProductController@updateProduct');
-Route::delete('products/{id}','ProductController@deleteProduct');
+
